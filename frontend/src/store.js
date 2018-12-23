9907d44655b08e24a2c49b8c4093b55642f69e78
Anderson Francisco <andsfran@gmail.com>
2018-12-23 20:04:46 -0200

    fix: Reducer location

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import postList from './pages/PostList/reducer';
 import singlePost from './pages/Post/reducer';
 import postForm from './pages/Post/form/reducer';
-import commentList from './components/Post/reducer';
+import commentList from './components/Comments/reducer';
 import editModal from './components/EditModal/reducer';
 import editCommentModal from './components/EditCommentModal/reducer';
 
